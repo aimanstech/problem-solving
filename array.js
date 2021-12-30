6003a39ddecf2cb5616e603036518977e9b3e5be
Aiman <aimanxtech@gmail.com>
2021-12-31 00:19:59 +0600

    array learn

---
+++ array.js
@@ -0,0 +1,30 @@
+// var fruits = ["apple", "kola", "orange", "pepe", 3,4,56, "alu"]
+
+// // fruits[0] = "banana" // changing a value in an array using it's index
+
+// // console.log(fruits.length) // length of an array
+
+// // console.log(fruits.indexOf("orange")) // indexOf is use to search an item is present or not in an array
+
+// fruits.pop(); // last item remove
+// fruits.push("strawberry"); // adding a new item at the end of an array
+// fruits.shift(); // remove the first element from the array
+// fruits.unshift("angur"); // add a item to the first element from an array
+
+// console.log(fruits);
+
+// 1 theke 10 number print kore diben 
+
+// for (var i = 1; i <= 10; i++){
+//     console.log(i)
+// }
+
+// 1 theke 10 number print kore diben while loop diye
+
+// var number = 1;
+
+// while(number <= 10){
+//     console.log(number);
+//     number++;
+// }
+
