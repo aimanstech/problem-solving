2a1fa37f71edce2fb028b373c0670bf889f103f0
Aiman <aimanxtech@gmail.com>
2022-01-03 03:06:13 +0600

    new array file added

---
+++ new_array.js
@@ -0,0 +1,53 @@
+// var vegetables = ["alu", "onion", 10, 9, "pepe", true, false]
+
+// var newSlice = vegetables.slice(3,13)
+// console.log(newSlice)
+
+// console.log(vegetables.length) // number of items in an array
+// console.log(vegetables.indexOf("onion")) // number of items in an array or search index of an item
+
+
+// console.log(vegetables[1]) // value of index
+// console.log(vegetables[7]) // value of index 
+
+
+// vegetables.push("tomato"); // adding a new item at the end of an array
+// vegetables.push("kola"); // adding a new item at the end of an array
+// vegetables.pop() // removes the item from end of an array
+// vegetables.pop()
+// vegetables.pop()
+// console.log(vegetables) 
+
+// vegetables.shift() // remove the items from the starting position of an array
+// vegetables.unshift("potato") // add the items at the starting position of an array
+// vegetables.unshift("capsicum") // add the items at the starting position of an array
+// console.log(vegetables)
+
+
+// sum of all items in an array 
+
+// var age = [10,15,47,22,56,35,45,5,66,4,2]
+// var sum = 0;
+
+// for (let i = 0; i < age.length; i++){
+//     sum = sum + age[i];
+// }
+// console.log(sum)
+
+// even or odd
+// for (let i = 0; i < age.length; i++){
+//     if(age[i] % 2 === 0){
+//         console.log(age[i] + " is even")
+//     } else {
+//         console.log(age[i] + " is odd")
+//     }
+// }
+
+
+var fruits = ["orange", "apple", "pepe", "banana", "strawberry", 12, 2 , 1]
+
+// console.log(fruits.sort())
+// console.log(fruits.reverse())
+
+var splice = fruits.splice(2,1)
+console.log(fruits)
