95da7a6f3cab90b0a86cdf9176bde221422749a5
Aiman <aimanxtech@gmail.com>
2022-01-08 04:06:02 +0600

    problem solving

---
+++ problem_solve.js
@@ -0,0 +1,34 @@
+
+// Without Using Function
+
+// var sentence = "Hello, good Morning - this is adam"
+// var sentenceLowercase = sentence.toLowerCase
+
+// var search = "adam";
+// var newSearch = search.toLowerCase()
+
+// if (sentenceLowercase.indexOf(newSearch) === -1) {
+//     console.log(sentenceLowercase.indexOf(newSearch), false)
+// } else {
+//     console.log(sentenceLowercase.indexOf(newSearch), true)
+// }
+
+// Using Function 
+
+// let sentence = "Hello, good Morning - this is adam"
+
+// function searchLetter(search) {
+//     var sentenceLowercase = sentence.toLowerCase();
+//     var newSearch = search.toLowerCase()
+//     var searchSentence = sentenceLowercase.indexOf(newSearch);
+
+//     if (searchSentence !== -1) {
+//         console.log(searchSentence, false)
+//         // return true;
+//     } else {
+//         console.log(searchSentence, true)
+//         // return false;
+//     }
+// }
+
+// searchLetter("Adam")
